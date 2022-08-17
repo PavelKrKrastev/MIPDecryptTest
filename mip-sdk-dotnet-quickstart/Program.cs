004969b851fb45cc691f335a199415b482119aca
Pavel Krastev <pkrastev@nemetschek.bg>
2022-08-17 22:03:59 +0300

    Initial commit

---
+++ mip-sdk-dotnet-quickstart/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+*
+* Copyright (c) Microsoft Corporation.
+* All rights reserved.
+*
+* This code is licensed under the MIT License.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files(the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions :
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*/
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.InformationProtection;
+using Microsoft.InformationProtection.File;
+using Microsoft.InformationProtection.Protection;
+using Microsoft.InformationProtection.Policy;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace MipSdkDotNetQuickstart
+{
+    class Program
+    {
+        private static readonly string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static readonly string appName = ConfigurationManager.AppSettings["app:Name"];
+        private static readonly string appVersion = ConfigurationManager.AppSettings["app:Version"];
+
+        static void Main(string[] args)
+        {
+
+            // Create ApplicationInfo, setting the clientID from Azure AD App Registration as the ApplicationId
+            // If any of these values are not set API throws BadInputException.
+            ApplicationInfo appInfo = new ApplicationInfo()
+            {
+                // ApplicationId should ideally be set to the same ClientId found in the Azure AD App Registration.
+                // This ensures that the clientID in AAD matches the AppId reported in AIP Analytics.
+                ApplicationId = clientId,
+                ApplicationName = appName,
+                ApplicationVersion = appVersion
+            };
+
+            // Initialize Action class, passing in AppInfo.
+            Action action = new Action(appInfo);
+
+            // Prompt for path inputs
+            Console.Write("Enter an input file path: ");
+            string inputFilePath = Console.ReadLine();
+
+            // Set file options from FileOptions struct. Used to set various parameters for FileHandler
+            Action.FileOptions options = new Action.FileOptions
+            {
+                FileName = inputFilePath,
+                ActionSource = ActionSource.Manual,
+                AssignmentMethod = AssignmentMethod.Standard,
+                DataState = DataState.Rest,
+                GenerateChangeAuditEvent = true,
+                IsAuditDiscoveryEnabled = true,
+                NotifyOwnerOnOpen = false
+            };
+
+            string result = action.DecryptFile(options);
+            Console.WriteLine(string.Format("Output file location {0}", result));
+            Console.WriteLine("Press a key to quit.");
+            Console.ReadKey();
+        }
+    }
+}
